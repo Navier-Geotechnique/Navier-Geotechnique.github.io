490b1105af66ed40147bb4e143d79ffb7233e4ec
mhpierre <max@tribupierre.net>
2025-10-06 22:47:47 +0200

    Paper retrieving

--- scripts/publications_retriever.py
+++ scripts/publications_retriever.py
@@ -4,6 +4,8 @@ Script to retrieve the labs publication from Crossref/Scholar
 """
 
 import requests
+from requests import auth
+from requests.auth import AuthBase
 import yaml
 
 with open("lab_list.yml", "r") as f:
@@ -20,9 +22,14 @@ for researcher in researchers:
             title = item.get("title", [""])[0]
             doi = item.get("DOI", "")
             year = item.get("created", {}).get("date-parts", [[None]])[0][0]
+            date = item.get("created", {}).get("date-parts")[0]
+            date_str = f"{date[0]}-{date[1]}-{date[2]}"
             authors = item.get("author", [])
+            journal = item.get("container-title", [""])[0]
+            issue = item.get("issue", "")
+            print(f"journal: {journal}")
             author_list = [
-                f"{a.get('given', '')} {a.get('family', '')}" for a in authors
+                f"{a.get('family', '')} {a.get('given', '')[0]}." for a in authors
             ]
             print("-----------------------------------------------------")
             print(f"{title} ({year}) {author_list} https://doi.org/{doi}")
@@ -30,14 +37,24 @@ for researcher in researchers:
             print("Abstract:")
             url = f"https://api.semanticscholar.org/graph/v1/paper/DOI:{doi}?fields=title,abstract,authors,year,url"
             abstract = requests.get(url).json().get("abstract")
-            abstract2 = item.get("abstract", "")
-            if abstract is not None:
-                print(abstract)
-            else:
-                print(abstract2)
+            if abstract is None:
+                abstract = item.get("abstract", "")
             print("-----------------------------------------------------")
             url = f"https://api.crossref.org/works/{doi}/transform/application/x-bibtex"
             bibtex = requests.get(url).text
             print(bibtex)
+            with open(f"../files/{doi.replace('/','-')}.bib", "w") as bib_file:
+                bib_file.write(bibtex)
 
-# Store all the data in a yaml file, then render .md files. Script updates the database, then re-renders, so database is always true and no duplication. Rendering layout can change.
+            filename = f"{doi.replace('/','-')}.md"
+
+            markdown_string = f'---\ntitle: "{title}"\ncollection: publications\ncategory: manuscripts\n'
+            markdown_string += f"permalink: /publications/{doi.replace('/','-')}\n"
+            markdown_string += f'date: {date_str}\nvenue: "{journal}"\n'
+            markdown_string += f'paperurl: "https://dx.doi.org/{doi}"\n'
+            # markdown_string += f'citation: "{author_list} ({year}) &quot;{title}.&quot; <i>{journal}</i>, {issue}"\n'
+            markdown_string += f"excerpt: By {", ".join(author_list)}.\n"
+            markdown_string += f"bibtexurl: http://navier-geotechnique.github.io/files/{doi.replace('/','-')}.bib\n---\n"
+            markdown_string += abstract
+            with open(f"../_publications/{filename}", "w") as md_file:
+                md_file.write(markdown_string)
